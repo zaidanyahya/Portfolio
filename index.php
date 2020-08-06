56fdea10f356e24850ee29430ac9ee3176159b72
zaidanyahya <35102388+zaidanyahya@users.noreply.github.com>
2020-08-06 17:56:49 +0900

    add index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Zaidan Yahya | ザイダン ヤーヤ</title>
+</head>
+<body>
+	<div class="main">
+		<div class="container">
+			<h1>Hi!</h1>
+			<h1>I'm Zaidan!</h1>
+		</div>
+	</div>
+</body>
+</html>
