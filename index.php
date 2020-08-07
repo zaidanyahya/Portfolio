a46128d6f300647489517efcccce2db409549167
Zaidan Yahya <zaidanyahya28@gmail.com>
2020-08-07 23:30:37 +0900

    add some basic stuff

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<title>Zaidan Yahya | ザイダン ヤーヤ</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
 </head>
 <body>
-	<div class="main">
-		<div class="container">
+	<section class="s1">
+		<div class="main-container">
 			<h1>Hi!</h1>
 			<h1>I'm Zaidan!</h1>
+			<h3><a href="#">Link</a></h3>
 		</div>
-	</div>
+	</section>
+
+	<section class="s2">
+		<div class="main-container">
+			<h2>About</h2>
+		</div>
+	</section>
+
+	<section class="s1">
+		<div class="main-container">
+			<h2>Portfolio</h2>
+		</div>
+	</section>
+
+	<section class="s2">
+		<div class="main-container">
+			<h2>Contact</h2>
+		</div>
+	</section>
+
+	<section class="s1 footer">
+		<div class="main-container">
+			<div class="content">
+				<div class="box">
+					<h4>Box 1</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+				</div>
+				<div class="box">
+					<h4>Box 2</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+				<div class="box">
+					<h4>Box 3</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+				</div>
+			</div>
+		</div>
+	</section>
 </body>
 </html>
