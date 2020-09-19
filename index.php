2416fcce75f0df91ae495f1de80f57f4f14dc195
Zaidan Yahya <zaidanyahya28@gmail.com>
2020-09-19 16:41:50 +0900

    edit intro

--- index.php
+++ index.php
@@ -6,34 +6,58 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
 </head>
 <body>
-	<section class="s1">
-		<div class="main-container">
-			<h1>Hi!</h1>
-			<h1>I'm Zaidan!</h1>
-			<h3><a href="#">Link</a></h3>
+	<section class="section-style1">
+		<div class="container">
+			<div class="greeting-wrapper">
+				<h1>Hi, I'm Zaidan!</h1>
+				<h3><a href="#">My Projects</a></h3>
+			</div>
+			<div class="intro-wrapper">
+				<div class="nav-wrapper">
+					<div class="dots">
+						<div id="dot-close" class="browser-dot"></div>
+						<div id="dot-min" class="browser-dot"></div>
+						<div id="dot-max" class="browser-dot"></div>
+					</div>
+					<ul id="navigation" >
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+				<div class="left-column">
+					<img id="profile-pic" src="assets/img/profile.png">
+					<a href="https://www.reddit.com/r/sololeveling/comments/h0rrgm/sung_jinwoo_igris_chibi_art_by_%E6%98%8E%E8%8F%9C/" target="_blank">Image Source</a>
+				</div>
+				<div class="right-column">
+					<div id="preview">
+						<h3>My Summary</h3>
+						<p>Simple description of me and what I do</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 
-	<section class="s2">
-		<div class="main-container">
+	<section class="section-style2">
+		<div class="container">
 			<h2>About</h2>
 		</div>
 	</section>
 
-	<section class="s1">
-		<div class="main-container">
+	<section class="section-style1">
+		<div class="container">
 			<h2>Portfolio</h2>
 		</div>
 	</section>
 
-	<section class="s2">
-		<div class="main-container">
+	<section class="section-style2">
+		<div class="container">
 			<h2>Contact</h2>
 		</div>
 	</section>
 
-	<section class="s1 footer">
-		<div class="main-container">
+	<section class="section-style1 footer">
+		<div class="container">
 			<div class="content">
 				<div class="box">
 					<h4>Box 1</h4>
